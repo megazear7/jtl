441dfdd75d72071123ef1e30c5217bb0948cef30
Alex Lockhart <alexlockahrt7@gmail.com>
2020-09-28 06:37:56 -0400

    Added minified versions of the distributed files

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+import minify from 'rollup-plugin-babel-minify';
 
 export default [{
     input: 'src/index.js',
@@ -39,5 +40,47 @@ export default [{
     output: {
         file: 'dist/jtl-esm.js',
         plugins: [ ]
+    }
+}, {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/jtl-umd.min.js',
+        format: 'umd',
+        name: 'jtl',
+        plugins: [
+            getBabelOutputPlugin({
+                presets: [
+                    [
+                        '@babel/preset-env',
+                        {
+                            "targets": {
+                                "ie": "11"
+                            }
+                        }
+                    ]
+                ],
+                allowAllFormats: true
+            }),
+            minify()
+        ]
+    }
+}, {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/jtl-cjs.min.js',
+        format: 'cjs',
+        name: 'jtl',
+        plugins: [
+            ["@babel/plugin-transform-modules-commonjs", {
+                "allowTopLevelThis": true
+            }],
+            minify()
+        ]
+    }
+}, {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/jtl-esm.min.js',
+        plugins: [ minify() ]
     }
 }];
