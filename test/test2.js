7d84b910e23c97168714dc114eca6352aea5641f
Alex Lockhart <alexlockahrt7@gmail.com>
2020-09-28 07:03:28 -0400

    Added support for self closing tags.

---
+++ test/test2.js
@@ -0,0 +1,27 @@
+import chai from 'chai';
+import { jtl } from '../src/index.js';
+
+const expect = chai.expect;
+
+describe('self closing tag test', function () {
+const json = {
+    name: "html",
+    attrs: {
+        "lang": "en"
+    },
+    children: [
+        { name: "head" },
+        {
+            name: "body",
+            children: [
+                { name: "br" }
+            ]
+        }
+    ]
+}
+
+  it('renders the html', function () {
+    expect(jtl(json).toHtmlString())
+    .eql(`<html lang="en"><head></head><body></br></body></html>`)
+  });
+});
